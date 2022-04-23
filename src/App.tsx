5d384c0fadc4f441fd1c536ab246b88c3c1dab1c
devanshdwivedi <devanshdwivedi@gmail.com>
2022-04-23 08:30:12 +0530

    Adding initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import GLOBAL_CONSTANTS from "./GlobalConstants";
+import Form from './components/Form';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <img className="upi-img" src={GLOBAL_CONSTANTS.UPI_IMAGE} alt="Payment graphic" />
+        <Form />
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Form.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <form>
+      <div className="button-container">
+        <button>Pay now</button>
+      </div>
+    </form>
+  );
+}
